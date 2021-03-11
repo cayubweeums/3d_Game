5f4571ab28ee5510cd3faa602ef2374321ef9fa0
Sammy Gaskins <sammycole@comcast.net>
2021-03-11 11:00:24 -0600

    Added warning sound if close to jellyfish

--- Assets/Scripts/JellyfishScripts/HuntPlayer.cs
+++ Assets/Scripts/JellyfishScripts/HuntPlayer.cs
@@ -6,8 +6,10 @@ public class HuntPlayer : MonoBehaviour
 {
     public GameObject player;
     private Rigidbody rigidbody;
+    private AudioSource source;
     public float viewDistance;
     public float moveSpeed;
+    public float soundDistance;
 
     private void Awake()
     {
@@ -18,6 +20,7 @@ public class HuntPlayer : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,6 +31,16 @@ public class HuntPlayer : MonoBehaviour
         if (playerInSight)
         {
             rigidbody.velocity = moveSpeed * (player.transform.position - this.transform.position).normalized;
+            if (Physics.Raycast(this.transform.position, player.transform.position - this.transform.position, soundDistance))
+            {
+                if (!source.isPlaying)
+                {
+                    source.Play();
+                }
+            } else
+            {
+                source.Stop();
+            }
         }
     }
     void OnDrawGizmos()
