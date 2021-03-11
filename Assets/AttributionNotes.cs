a3a0bf4a4f022b3dacb163aef34a62597f16dd37
Sammy Gaskins <sammycole@comcast.net>
2021-03-10 22:55:55 -0600

    Added menu

---
+++ Assets/AttributionNotes.cs
@@ -0,0 +1,9 @@
+﻿/*
+ Clam and pearl model designed by Addi_Artwork, found at:
+https://sketchfab.com/3d-models/clam-pearl-bb81efe5cfaa40bfacfacdc46db1ff7b
+Simple Jellyfish modelf designed by ricksticky, found at:
+https://sketchfab.com/3d-models/simple-jellyfish-f77876d8297846eeb23c4ad82dbebb97
+Menu background by Ciera Elizabeth Hoover [SugarMoonWitch]:
+- website: http://sugarmoonwitch.com/
+- image source: https://opengameart.org/content/underwater-bg
+ */
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +13,16 @@ public class GameManager : MonoBehaviour
     public GameObject events;
 
     public GameObject startButton;
+    public GameObject instructionButton;
+    public GameObject creditsButton;
+    public GameObject backButton;
+
+    public GameObject titleTextBox;
+    public GameObject instructionTextBox;
+    public GameObject creditsTextBox;
+
+    public GameObject backgroundImage;
+    public GameObject blackBackground;
 
     private void Awake()
     {
@@ -44,13 +55,62 @@ public class GameManager : MonoBehaviour
 
     public void StartButton()
     {
-        startButton.SetActive(false);
+        DeactivateAllButtons();
+        titleTextBox.SetActive(false);
         StartCoroutine(LoadYourAsyncScene("SampleScene"));
+        // Fade image out
+        StartCoroutine(FadeBackgroundImage(new Color(1, 1, 1, 0), 2));
+        backgroundImage.SetActive(false);
+    }
+
+    public void CreditsButton()
+    {
+        DeactivateAllButtons();
+        titleTextBox.SetActive(false);
+        backButton.SetActive(true);
+        creditsTextBox.SetActive(true);
+    }
+
+    public void InstructionButton()
+    {
+        DeactivateAllButtons();
+        titleTextBox.SetActive(false);
+        backButton.SetActive(true);
+        instructionTextBox.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        creditsTextBox.SetActive(false);
+        instructionTextBox.SetActive(false);
+        backButton.SetActive(false);
+        ActivateMenuButtons();
+        titleTextBox.GetComponent<TextMeshProUGUI>().SetText("Game Title");
+        titleTextBox.SetActive(true);
     }
 
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        titleTextBox.GetComponent<TextMeshProUGUI>().SetText("Game Over");
+        EndGame();
+    }
+
+    public void WinGame()
+    {
+        titleTextBox.GetComponent<TextMeshProUGUI>().SetText("You Win!");
+        EndGame();
+    }
+
+    private void EndGame()
+    {
+        blackBackground.SetActive(true);
+        StartCoroutine(LoadYourAsyncScene("Menu"));
+        backButton.SetActive(true);
+        titleTextBox.SetActive(true);
+        // Fade image in
+        backgroundImage.SetActive(true);
+        StartCoroutine(FadeBackgroundImage(new Color(1, 1, 1, 1), 2));
+        Cursor.lockState = CursorLockMode.None;
     }
 
     IEnumerator LoadYourAsyncScene(string scene)
@@ -63,5 +123,36 @@ public class GameManager : MonoBehaviour
         {
             yield return null;
         }
+    }
+
+    IEnumerator FadeBackgroundImage(Color endValue, float duration)
+    {
+        float time = 0;
+        Image sprite = backgroundImage.GetComponent<Image>();
+        Color startValue = sprite.color;
+
+        while (time < duration)
+        {
+            sprite.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        sprite.color = endValue;
+        blackBackground.SetActive(false);
+    }
+
+    private void ActivateMenuButtons()
+    {
+        startButton.SetActive(true);
+        creditsButton.SetActive(true);
+        instructionButton.SetActive(true);
+    }
+
+    private void DeactivateAllButtons()
+    {
+        startButton.SetActive(false);
+        creditsButton.SetActive(false);
+        instructionButton.SetActive(false);
+        backButton.SetActive(false);
     }
 }
