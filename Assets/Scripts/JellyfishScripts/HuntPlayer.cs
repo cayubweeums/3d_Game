7556f69ece7d3a4def26a270b75be2af1b8364de
ericpinter <48108495+ericpinter@users.noreply.github.com>
2021-03-10 21:28:11 -0600

    Jellyfish flock toward player

---
+++ Assets/Scripts/JellyfishScripts/HuntPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuntPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Rigidbody rigidbody;
+    public float viewDistance;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool playerInSight = Physics.Raycast(this.transform.position, player.transform.position - this.transform.position, viewDistance);
+        Debug.Log("player in sight " + playerInSight);
+        if (playerInSight)
+        {
+            rigidbody.velocity = moveSpeed * (player.transform.position - this.transform.position).normalized;
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, rigidbody.velocity);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit Something");
+        if (collision.collider.name == "Player")
+        {
+            GameManager.Instance.GameOver();
+        }
+    }
+}
