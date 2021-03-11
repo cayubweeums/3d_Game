25993c4281e60de1550c53a4b8eb9eee572c5b24
ericpinter <48108495+ericpinter@users.noreply.github.com>
2021-03-10 20:13:39 -0600

    Changes to movement and camera movement

--- Assets/Scripts/RotateWithMouse.cs
+++ Assets/Scripts/RotateWithMouse.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class RotateWithMouse : MonoBehaviour
 {
+    //based on https://gamedev.stackexchange.com/questions/104693/how-to-use-input-getaxismouse-x-y-to-rotate-the-camera
     public float mouseSensitivity;
-    public GameObject player;
-    private Transform playerTransform;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerTransform = player.GetComponent<Transform>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        yaw += mouseSensitivity * Input.GetAxis("Mouse X") * Time.deltaTime;
+        pitch -= mouseSensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime;
+        //lock vertical movement to 70 degrees
+        pitch = Mathf.Clamp(pitch,-70, 70);
+        Debug.Log(pitch);
 
-        Debug.Log(mouseY);
-        
-        playerTransform.Rotate(mouseY, mouseX, 0f);
+        this.transform.eulerAngles = new Vector3(pitch, yaw) ;
         
     }
 }
